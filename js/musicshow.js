d099212338fa71989f00acc873de9cb90105db2f
tomorrow505 <tomorrow135@hust.edu.cn>
2021-04-09 14:01:58 +0800

    Site updated: 2021-04-09 14:01:52

--- js/musicshow.js
+++ js/musicshow.js
@@ -1,17 +1,16 @@
 !(function() {
   function show() {
+     //简单判断首页，因为我的域名是tomorrow50.xyz，所以首页就会触发
 	if (location.href.match(/xyz.{0,1}$/)){
-		//滚动条事件
+         //直接把音乐框隐藏
 		$("#music_div").attr("style","display:none;");
+         //滚动条事件
 		$(window).scroll(function(){
 			//获取滚动条的滑动距离
 			var scroH = $(this).scrollTop();
-			console.log(scroH);
-			// console.log(scroH);
-			//滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
+			//滚动条的滑动距离大于120，就显示，反之就隐藏
 			if(scroH >= 120){
 				$("#music_div").attr("style","display:block;position:fixed;bottom:0px;left:30px;");
-				$("#music_div").css({});
 			}else if(scroH < 120){
 				$("#music_div").attr("style","display:none;");
 			}
