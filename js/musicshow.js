6623052a660737c785d52c06ed018751b75424fc
tomorrow505 <tomorrow135@hust.edu.cn>
2021-04-09 10:33:19 +0800

    Site updated: 2021-04-09 10:33:19

--- js/musicshow.js
+++ js/musicshow.js
@@ -1,6 +1,6 @@
 !(function() {
   function show() {
-	if (location.href === "https://tomorrow505.xyz/"){
+	if (location.href.match(/xyz.{0,1}$/)){
 		//滚动条事件
 		$("#music_div").attr("style","display:none;");
 		$(window).scroll(function(){
