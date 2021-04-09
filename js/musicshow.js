391f6fc74f766a7177d4a00a39e1ac902298bda9
tomorrow505 <tomorrow135@hust.edu.cn>
2021-04-09 10:17:43 +0800

    Site updated: 2021-04-09 10:17:42

--- js/musicshow.js
+++ js/musicshow.js
@@ -11,8 +11,10 @@
 			//滚动条的滑动距离大于等于定位元素距离浏览器顶部的距离，就固定，反之就不固定
 			if(scroH >= 120){
 				$("#music_div").attr("style","display:block;");
+				$("#music_div").css({"position": "fixed", "bottom": "0px", "left": "30px"});
 			}else if(scroH < 120){
 				$("#music_div").attr("style","display:none;");
+				$("#music_div").css({"position": "fixed", "bottom": "0px", "left": "30px"});
 			}
 		 })
 	}
