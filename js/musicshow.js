0d3b8b811732599e4135aeb2bf5febe1c0e7e129
tomorrow505 <tomorrow135@hust.edu.cn>
2021-04-09 20:49:37 +0800

    Site updated: 2021-04-09 20:49:32

--- js/musicshow.js
+++ js/musicshow.js
@@ -10,7 +10,7 @@
 			var scroH = $(this).scrollTop();
 			//滚动条的滑动距离大于120，就显示，反之就隐藏
 			if(scroH >= 120){
-				$("#music_div").attr("style","display:block;position:fixed;bottom:0px;left:30px;");
+				$("#music_div").attr("style","display:block;position:fixed;bottom:0px;left:30px; z-index: 100");
 			}else if(scroH < 120){
 				$("#music_div").attr("style","display:none;");
 			}
